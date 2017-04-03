13195153d9bc9c1c6873dcc7babbdc4450b1bd7b
Morgan ROBIN <morgan@robin.wf>
2017-04-03 13:08:21 +0200

    Refactor the whole post module

--- post/admin.py
+++ post/admin.py
@@ -1,15 +1,19 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
 from django.contrib import admin
 from .models import Annonce, Domaine, Zone, TypeContrat, Organisme, Duree
+from django.contrib.auth.models import Group
 
 # Classe qui sert à mettre en forme la gestion des annonces
 class AnnonceAdmin(admin.ModelAdmin):
     fieldsets = [
-	('Informations indispensables', 			{'fields': ['zone', 'nom_entreprise', 'type_de_contrat', 'duree', 'domain']}),
-	("Accès à l'annonce - Renseignez au moins un champ", 	{'fields': ['url_annonce', 'fichier', 'multiple_files']}),
-	('Informations optionnelles',				{'fields': ['ville', 'date_debut', 'type_organisme', 'commentaires']}),
+	   ('Informations indispensables', {
+            'fields': ['zone', 'nom_entreprise', 'type_de_contrat', 'duree', 'domain']
+        }),
+	   ("Accès à l'annonce - Renseignez au moins un champ", {
+            'fields': ['url_annonce', 'fichier', 'multiple_files']
+        }),
+	   ('Informations optionnelles', {
+            'fields': ['ville', 'date_debut', 'type_organisme', 'commentaires']
+        }),
     ]
     list_display = ('id', 'nom_entreprise', 'zone', 'type_de_contrat', 'domaines', 'pub_date', 'visites') 
     list_filter = ['pub_date', 'type_de_contrat', 'zone', 'domain']
@@ -25,3 +29,4 @@ admin.site.register(Zone)
 admin.site.register(TypeContrat)
 admin.site.register(Organisme)
 admin.site.register(Duree)
+admin.site.unregister(Group)
--- post/tests.py
+++ post/tests.py
@@ -1,3 +1,55 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from django.core.urlresolvers import reverse
 
-# Create your tests here.
+from .forms import AnnonceForm
+from .models import Zone, Duree, Domaine, TypeContrat
+
+class PostForms(TestCase):
+    def setUp(self):
+        Zone.objects.create(nom='île-de-france')
+        Duree.objects.create(duree='6 mois')
+        Domaine.objects.create(nom='Télécoms')
+        TypeContrat.objects.create(type_contrat='stage')
+
+    def test_annonce(self):
+        form = AnnonceForm(data={
+            'zone': 1,
+            'duree': 1,
+            'domain': [1],
+            'nom_entreprise': 'orange',
+            'type_de_contrat': 1,
+            'multiple_files': 0,
+        })
+        self.assertTrue(form.is_valid())
+
+class PostViews(TestCase):
+    def setUp(self):
+        User.objects.create_user('john', 'john@smith.com', 'password', is_staff=True)
+        Zone.objects.create(nom='île-de-france')
+        Duree.objects.create(duree='6 mois')
+        Domaine.objects.create(nom='Télécoms')
+        TypeContrat.objects.create(type_contrat='stage')
+
+    def test_create_annonce_login(self):
+        response = self.client.get(reverse('post:creation'))
+        self.assertEquals(response.status_code, 302)
+        self.client.login(username='john', password='password')
+        response = self.client.get(reverse('post:creation'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'post/creation.html')
+
+    def test_create_annonce_normal(self):
+        self.client.login(username='john', password='password')
+        response = self.client.post(
+            reverse('post:creation'), 
+            {
+                'zone': 1,
+                'duree': 1,
+                'domain': [1],
+                'nom_entreprise': 'orange',
+                'type_de_contrat': 1,
+                'multiple_files': 0,
+            }
+        )
+        self.assertEqual(response.status_code, 302)
