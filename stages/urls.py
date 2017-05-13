ee6121a2b506612652278433b08303b127391c48
Morgan ROBIN <morgan@robin.wf>
2017-05-13 14:51:20 +0200

    Serve media files in dev, and add a template to login through django database

--- stages/urls.py
+++ stages/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django_cas_ng import views as cas_views
+from django.conf.urls.static import static
+from django.conf import settings
 
 from search.views import IndexView
 
@@ -15,3 +17,6 @@ urlpatterns = [
 	url(r'^post/', include('post.urls', namespace="post")),
 	url(r'^search/', include('search.urls', namespace="search")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
