fb436f8b43a8882140e86188c9fc6131476231a6
Morgan ROBIN <morgan@robin.wf>
2017-05-15 22:56:24 +0200

    Tricky thing thoses dicts !

--- pages/templatetags/url_replace.py
+++ pages/templatetags/url_replace.py
@@ -4,7 +4,7 @@ from django import template
 register = template.Library()
 
 @register.simple_tag(takes_context=True)
-def url_replace(context, **kwargs):
+def url_replace(context, page):
     query = context['request'].GET.dict()
-    query.update(kwargs)
+    query.update({'page': page})
     return urlencode(query)
