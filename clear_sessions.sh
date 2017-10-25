c1b58f58591ed473cf5cb89cb36ae2356f985188
Morgan ROBIN <morgan@robin.wf>
2017-10-25 12:08:44 +0200

    Improve display

--- clear_sessions.sh
+++ clear_sessions.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 cd /srv/www/stages.resel.fr
+echo '>>> Cleaning Django sessions\n'
 /srv/www/stages.resel.fr/env/bin/python manage.py clearsessions
-/srv/www/stages.resel.fr/env/bin/python manage.py django_cas_ng_clean_sessions 
+echo '[executed]\n'
+
+echo '\n>>> Cleaning django-cas-ng sessions\n'
+/srv/www/stages.resel.fr/env/bin/python manage.py django_cas_ng_clean_sessions
+echo '[executed]'
