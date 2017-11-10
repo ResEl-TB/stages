e776eb3b982285fb69033ca0850e9fb6b4d8b620
Morgan ROBIN <morgan@robin.wf>
2017-11-10 11:42:42 +0100

    Rename a usefull command

--- post/management/commands/clean_announces.py
+++ post/management/commands/clean_announces.py
@@ -1,18 +1,16 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
 
 from datetime import timedelta
 
 from django.core.management.base import BaseCommand, CommandError
 from django.utils import timezone
 from django.utils.dateformat import DateFormat
-from django.core.mail import send_mail
 from django.core.files import File
 
 from post.models import Annonce
 
 class Command(BaseCommand):
-    help = 'Supprime une annonce si sa durée de vie est dépassée.'
+    help = 'clean the old announces (older than 3 months)'
 
     def handle(self, *args, **options):
         annonces = Annonce.objects.all()
